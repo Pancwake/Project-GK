18142e5dd526ea3bae66ccf27d4a7ccb729238ac
Pancwake <raphael.zierl@gmail.com>
2026-05-11 18:16:41 +0200

    Furthered catching logic
    There is a percentage of the ball that you have to press in the middle now for it to count as a catch. Otherwise it still goes in (Change that to repelling)

--- Project GK/Assets/Project/Scripts/CatchHandler.cs
+++ Project GK/Assets/Project/Scripts/CatchHandler.cs
@@ -17,6 +17,7 @@ public class CatchHandler : MonoBehaviour
     [SerializeField] bool isCatching; //If the player is currently trying to catch
     [SerializeField] float catchTime; //How early the button can be pressed to still count as a catch
     [SerializeField] float catchCooldown; //How long the player has to wait before being able to catch again (Anti spam)
+    [SerializeField] [Range(0f, 100f)] int catchAreaPercentage; //The percentage of the area that the ball can be caught in
     Coroutine catchTimerCoroutine;
     Coroutine catchCooldownCoroutine;
 
@@ -96,8 +97,16 @@ public class CatchHandler : MonoBehaviour
     bool TryCatching()
     {
         //Add center check
+        Vector3 hitPosition = hit.point;
+        hitPosition.z = hit.transform.position.z; //Level it with the ball for distance to middle check
 
-        return isCatching;
+        var ballRadius = hit.collider.bounds.extents.x;
+        var distance = Vector3.Distance(hitPosition, hit.transform.position); //Distance from where hit to the middle of the ball
+        var maxDistanceToCatch = ballRadius * (float)(catchAreaPercentage / 100f); //The max distance the mouse can be away to allow a catch
+
+        Debug.Log("Catch: " + distance + " <= " + maxDistanceToCatch + " = " + (distance <= maxDistanceToCatch));
+
+        return distance <= maxDistanceToCatch; //If distance is in the catch threshhold
     }
 
     public void CatchBall()
